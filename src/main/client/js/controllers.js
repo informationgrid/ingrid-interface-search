d4f1eec07badc3d291a63f19132e84aed4e1d27e
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-04-11 17:16:45 +0000

    - added angularjs-webapp with support to show service-feeds and its dataset-feeds (REDMINE-348)

---
+++ src/main/client/js/controllers.js
@@ -0,0 +1,83 @@
+function AtomCtrl($scope, $http) {
+    a = $scope;
+    $scope.oneAtATime = false;
+    $scope.entries = [];
+    $scope.feeds = [];
+    $scope.datasetLoaded = [];
+    $scope.subsetsLoaded = true;
+    console.log("AtomCtrl");
+    
+    $http.get( "service-list" ).then(function(response) {
+        var feeds = response.xml.find("entry");
+        angular.forEach(feeds, function(feed) {
+            var element = angular.element(feed);
+            var title = element.find("title")[0].innerHTML;
+            var summary = element.find("summary")[0].innerHTML;
+            var date = element.find("updated")[0].innerHTML;
+            var link = feed.querySelector("[rel=alternate]").getAttribute("href");
+            $scope.feeds.push( {
+                title: title,
+                summary: summary,
+                date: date,
+                link: link
+            });
+        });
+        $scope.feedsLoaded = true;
+    });
+    
+    
+    $scope.showDatasetFeeds = function() {
+        console.log("changed:", $scope.selectedFeed);
+        $scope.entries = [];
+        $scope.subsetsLoaded = false;
+        $http.get( $scope.selectedFeed.link ).then(function(response) {
+            var feedId = response.xml.find("id")[0].innerHTML;
+            var entriesDom = response.xml.find("entry");
+            angular.forEach(entriesDom, function(entry) {
+                var element = angular.element(entry);
+                var title = element.find("title")[0].innerHTML;
+                var summary = element.find("summary")[0].innerHTML;
+                var date = element.find("updated")[0].innerHTML;
+                var link = entry.querySelector("[rel=alternate]").getAttribute("href");
+                $scope.entries.push( {
+                    title: title,
+                    summary: summary,
+                    date: date,
+                    link: link,
+                    useConstraints: "???"
+                });
+            });
+            $scope.subsetsLoaded = true;
+            $scope.datasetLoaded = [];
+            
+        }, function(error) {
+            //console.error(error);
+        });
+    };
+    
+    $scope.loadDownloadsFeed = function(dsEntry, isopen, index) {
+        if (!$scope.datasetLoaded[index]) {
+            console.log("loadDownloadsFeed:", dsEntry);
+            $http.get( dsEntry.link ).then(function(response) {
+                dsEntry.useConstraints = response.xml.find("rights")[0].innerHTML;
+                var entriesDom = response.xml.find("entry");
+                dsEntry.downloads = [];
+                angular.forEach(entriesDom, function(entry) {
+                    var linkElem = entry.querySelector("[rel=alternate]");
+                    var link = linkElem.getAttribute("href");
+                    var fileType = linkElem.getAttribute("type");
+                    var crs = entry.querySelector("category").getAttribute("label");
+                    
+                    dsEntry.downloads.push({
+                        link: link,
+                        fileType: fileType,
+                        crs: crs
+                    });
+                });
+                
+                $scope.datasetLoaded[index] = true;                
+            });
+        }
+    }
+    
+}
