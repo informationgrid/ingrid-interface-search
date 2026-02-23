d98348b00ae69626000b323ee1c2adc0fef1e0a6
André Wallat <andre.wallat@wemove.com>
2026-02-23 22:46:28 +0100

    Add `AddressWrapper` to represent location data in `Agent` model

--- src/main/java/de/ingrid/iface/opensearch/model/dcatapde/catalog/Agent.java
+++ src/main/java/de/ingrid/iface/opensearch/model/dcatapde/catalog/Agent.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -46,6 +46,9 @@ public class Agent {
 
     @JacksonXmlProperty(isAttribute = true, namespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
     private String about;
+
+    @JacksonXmlProperty(namespace = "http://www.w3.org/ns/locn#")
+    private AddressWrapper address;
 
     public String getName() {
         return name;
@@ -85,5 +88,13 @@ public class Agent {
 
     public void setMbox(String mbox) {
         this.mbox = mbox;
+    }
+
+    public AddressWrapper getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressWrapper address) {
+        this.address = address;
     }
 }
